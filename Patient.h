0001f7b5aa67e2a502d1c79a2abc58ef3dfd0ba1
david.nguyen@smartsheet.com <david.nguyen@smartsheet.com>
2021-11-08 14:45:38 -0800

    add all base files

---
+++ Patient.h
@@ -0,0 +1,14 @@
+//
+// Created by dzzn on 11/8/2021.
+//
+
+#ifndef P3_PATIENT_H
+#define P3_PATIENT_H
+
+
+class Patient {
+
+};
+
+
+#endif //P3_PATIENT_H
---
+++ PatientPriorityQueue.h
@@ -0,0 +1,14 @@
+//
+// Created by dzzn on 11/8/2021.
+//
+
+#ifndef P3_PATIENTPRIORITYQUEUE_H
+#define P3_PATIENTPRIORITYQUEUE_H
+
+
+class PatientPriorityQueue {
+
+};
+
+
+#endif //P3_PATIENTPRIORITYQUEUE_H
