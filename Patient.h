1a5a5b803cb700126944e0a8a8818da2b0c16bf3
david.nguyen@smartsheet.com <david.nguyen@smartsheet.com>
2021-11-09 02:29:01 -0800

    add Heap file example

--- Patient.h
+++ Patient.h
@@ -4,15 +4,22 @@
 
 #ifndef P3_PATIENT_H
 #define P3_PATIENT_H
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
 
 
 class Patient {
+public:
+    string to_string();
 
 private:
-    struct Priority{
-        int level;
+    //variables
+    string name;
+    string priorityCode;
+    int arrivalOrder;
 
-    };
 };
 
 
